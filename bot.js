a2f5fe74372c83883bc460b085b6c99ca09f5083
1Hamad1 <44902218+1Hamad1@users.noreply.github.com>
2018-11-16 11:06:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Gaft ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`iLoveLegend <3 `,"http://twitch.tv/1legaft")
+client.user.setGame(`iLoveLegend <3 `,"http://twitch.tv/iLoveLegend <3")
 client.user.setStatus("dnd")
 });
 
